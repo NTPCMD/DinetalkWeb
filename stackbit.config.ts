8e15329268ed2ca99210ed655f0333ddfcf863b9
NTPCMD <newtownpunjabi@gmail.com>
2025-11-07 00:19:15 +0800

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,6 +1,32 @@
-// stackbit.config.ts
 import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-  siteMap: () => [],
+  stackbitVersion: "~0.6.0",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["src/content"], // your home.json path
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "src/content/pages/{slug}.json",
+          fields: [
+            { name: "title", type: "string", required: true },
+            { name: "hero", type: "object" },
+            { name: "features", type: "object" },
+            { name: "cta", type: "object" }
+          ],
+        },
+      ],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "public",
+        uploadDir: "images",
+        publicPath: "/",
+      },
+    }),
+  ],
 });
